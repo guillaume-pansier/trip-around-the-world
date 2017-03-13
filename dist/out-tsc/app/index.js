1c8abcc8e6615a2f47a38447dff228861192687e
Guillaume Pansier <guillaume.pansier@axa.be>
2017-03-13 22:54:25 +0100

    remove dist folder from SVC

--- dist/out-tsc/app/index.js
+++
@@ -1,3 +0,0 @@
-export * from './app.component';
-export * from './app.module';
-//# sourceMappingURL=C:/Users/Guillaume/Desktop/angular2/ponyracer/src/app/index.js.map
--- dist/out-tsc/environments/environment.prod.js
+++
@@ -1,4 +0,0 @@
-export var environment = {
-    production: true
-};
-//# sourceMappingURL=C:/Users/Guillaume/Desktop/angular2/ponyracer/src/environments/environment.prod.js.map
